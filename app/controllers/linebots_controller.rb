6faedc6d69a11626638be44a0c1ac0ae4d73ec85
appleRui <apple31581@gmail.com>
2020-09-01 16:15:33 +0900

    edit controller

--- app/controllers/linebots_controller.rb
+++ app/controllers/linebots_controller.rb
@@ -1,50 +1,51 @@
 class LinebotsController < ApplicationController
 	require 'line/bot'
-	protect_from_forgery except: [:callback] # CSRF対策無効化
 
-    def callback
-      body = request.body.read
-      signature = request.env['HTTP_X_LINE_SIGNATURE']
+  # callbackアクションのCSRFトークン認証を無効
+  protect_from_forgery except: [:callback]
 
-      unless client.validate_signature(body, signature)
-        return head :bad_request
-      end
-
-      events = client.parse_events_from(body)
-
-      events.each do |event|
-        case event
-        when Line::Bot::Event::Message
-          case event.type
-          when Line::Bot::Event::MessageType::Text
-          	input = event.message['text']
-          	message = search_and_create_message(input)
-
-            client.reply_message(event['replyToken'], message)
-          end
+  def callback
+    body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      return head :bad_request
+    end
+    events = client.parse_events_from(body)
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          # 入力した文字をinputに格納
+          input = event.message['text']
+          # search_and_create_messageメソッド内で、楽天APIを用いた商品検索、メッセージの作成を行う
+          message = search_and_create_message(input)
+          client.reply_message(event['replyToken'], message)
         end
       end
-
-      "OK"
     end
+    head :ok
+  end
 
-    private
+  private
 
-    def client
-      @client ||= Line::Bot::Client.new { |config|
-        config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-        config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-      }
+  def client
+    @client ||= Line::Bot::Client.new do |config|
+      config.channel_secret = ENV['LINE_BOT_CHANNEL_SECRET']
+      config.channel_token = ENV['LINE_BOT_CHANNEL_TOKEN']
     end
+  end
 
-    def search_and_create_message(input)
+  def search_and_create_message(input)
     RakutenWebService.configure do |c|
       c.application_id = ENV['RAKUTEN_APPID']
       c.affiliate_id = ENV['RAKUTEN_AFID']
     end
-
+    # 楽天の商品検索APIで画像がある商品の中で、入力値で検索して上から3件を取得する
+    # 商品検索+ランキングでの取得はできないため標準の並び順で上から3件取得する
     res = RakutenWebService::Ichiba::Item.search(keyword: input, hits: 3, imageFlag: 1)
     items = []
+    # 取得したデータを使いやすいように配列に格納し直す
     items = res.map{|item| item}
     make_reply_content(items)
   end
